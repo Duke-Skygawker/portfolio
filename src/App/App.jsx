94e29ff441fdf323e57152197bdbedd77fd6bbdc
Duke-Skygawker <bart3kk990@gmail.com>
2024-02-13 07:50:12 +0100

    initialized navbar

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -3,11 +3,13 @@ import Certifications from "./components/Certifications/Certifications";
 import Hero from "./components/Hero/Hero";
 import Projects from "./components/Projects/Projects";
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
       <h1>This the app innit</h1>
+      <Navbar />
       <Hero />
       <About />
       <Projects />

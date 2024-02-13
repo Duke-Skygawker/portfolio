5b12e4809800b3980054a77fdb582f424081f580
Duke-Skygawker <bart3kk990@gmail.com>
2024-02-13 06:31:45 +0100

    initial commit

---
+++ src/App/App.jsx
@@ -0,0 +1,19 @@
+import About from "./components/About/About";
+import Certifications from "./components/Certifications/Certifications";
+import Hero from "./components/Hero/Hero";
+import Projects from "./components/Projects/Projects";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>This the app innit</h1>
+      <Hero />
+      <About />
+      <Projects />
+      <Certifications />
+    </>
+  );
+}
+
+export default App;
---
+++ src/App/components/Certifications/Certifications.jsx
@@ -0,0 +1,4 @@
+const Certifications = () => {
+  return <h1>Certifications</h1>;
+};
+export default Certifications;

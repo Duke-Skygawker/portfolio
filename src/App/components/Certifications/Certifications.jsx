fcd9e98e1a8c3412147c814ac207fa0c23cb9d24
Duke-Skygawker <bart3kk990@gmail.com>
2024-02-14 06:31:22 +0100

    certifications layout update

--- src/App/components/Certifications/Certifications.jsx
+++ src/App/components/Certifications/Certifications.jsx
@@ -4,22 +4,24 @@ const Certifications = () => {
   return (
     <div className="certifications" id="certifications">
       <h1>Certifications</h1>
-      <div className="cert-grid">
-        <img
-          src={cert1}
-          alt="Responsive Web Design Certification from freeCodeCamp"
-          className="cert-img"
-        />
+      <div>
+        <div className="cert-grid">
+          <img
+            src={cert1}
+            alt="Responsive Web Design Certification from freeCodeCamp"
+            className="cert-img"
+          />
+        </div>
+        <h4 className="cert-heading">
+          Currently working on:{" "}
+          <span className="cert-span">
+            Algorithms and Data Structures from{" "}
+            <a href="https://www.freecodecamp.org" target="_blank">
+              freeCodeCamp
+            </a>
+          </span>
+        </h4>
       </div>
-      <h4>
-        Currently working on:{" "}
-        <span className="cert-span">
-          Algorithms and Data Structures from{" "}
-          <a href="https://www.freecodecamp.org" target="_blank">
-            freeCodeCamp
-          </a>
-        </span>
-      </h4>
     </div>
   );
 };

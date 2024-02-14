fc848685f560f901bd86616fafe2e52c3aae8f1f
Duke-Skygawker <bart3kk990@gmail.com>
2024-02-14 06:17:15 +0100

    certifications done

--- src/App/components/Certifications/Certifications.jsx
+++ src/App/components/Certifications/Certifications.jsx
@@ -1,7 +1,16 @@
+import cert1 from "../../../assets/certs/freecodecamp_responsive_web_cert_censored.jpg";
+
 const Certifications = () => {
   return (
     <div className="certifications" id="certifications">
       <h1>Certifications</h1>
+      <div className="cert-grid">
+        <img
+          src={cert1}
+          alt="Responsive Web Design Certification from freeCodeCamp"
+          className="cert-img"
+        />
+      </div>
     </div>
   );
 };

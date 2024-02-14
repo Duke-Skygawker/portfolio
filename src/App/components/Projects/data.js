f998947cbd85a4d7eb75c04a4d6f8b81c4743978
Duke-Skygawker <bart3kk990@gmail.com>
2024-02-14 09:34:03 +0100

    added projects data

---
+++ src/App/components/Projects/data.js
@@ -0,0 +1,38 @@
+const projects = [
+  {
+    name: "Social Media Dashboard with a theme switcher",
+    img: "bla",
+    repo: "https://github.com/Duke-Skygawker/social-media-dashboard-with-theme-switcher",
+    url: "https://duke-skygawker.github.io/social-media-dashboard-with-theme-switcher/",
+  },
+  {
+    name: "Blackjack; Player vs Dealer",
+    img: "bla",
+    repo: "https://github.com/Duke-Skygawker/Blackjack-player-vs-dealer",
+    url: "https://duke-skygawker.github.io/Blackjack-player-vs-dealer/",
+  },
+  {
+    name: "Dynamic Portfolio with Bootstrap 5 & SASS",
+    img: "bla",
+    repo: "https://github.com/Duke-Skygawker/Bootstrap-5-and-SASS-dynamic-portfolio",
+    url: "#NEED_TO_HOST",
+  },
+  {
+    name: "Shopping cart in React using a reducer",
+    img: "bla",
+    repo: "https://github.com/Duke-Skygawker/shopping-cart-react-reducer",
+    url: "https://react-cart-63e6b7.netlify.app",
+  },
+  {
+    name: "Backroads App; A Tourism company home page ",
+    img: "bla",
+    repo: "https://github.com/Duke-Skygawker/backroads-app",
+    url: "https://backroads-app-67f1fc.netlify.app",
+  },
+  {
+    name: "Color shades generator",
+    img: "bla",
+    repo: "https://github.com/Duke-Skygawker/color-generator-react",
+    url: "https://stirring-sunshine-e5f1cf.netlify.app",
+  },
+];

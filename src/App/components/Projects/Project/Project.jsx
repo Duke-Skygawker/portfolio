6ee6c7d5e382bc6b3f1e2eecb5813d40481871a3
Duke-Skygawker <bart3kk990@gmail.com>
2024-02-15 06:21:42 +0100

    added links to the project card

--- src/App/components/Projects/Project/Project.jsx
+++ src/App/components/Projects/Project/Project.jsx
@@ -2,8 +2,14 @@ const Project = ({ item }) => {
   const { name, img, repo, url } = item;
   return (
     <div className="project-card">
-      <img src={img} alt={name} />
-      <h3>{name}</h3>
+      <a href={url} target="_blank">
+        <img src={img} alt={name} />
+      </a>
+      <h3>
+        <a href={repo} target="_blank">
+          {name}
+        </a>
+      </h3>
     </div>
   );
 };

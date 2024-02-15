1cafad5517efc838eb119672a328505d8c281ef2
Duke-Skygawker <bart3kk990@gmail.com>
2024-02-15 05:04:41 +0100

    added images to the data

--- src/App/components/Projects/data.js
+++ src/App/components/Projects/data.js
@@ -1,37 +1,44 @@
+import SocialMediaDash from "../../../assets/images/PORTFOLIO PROJECTS/social_media_dashboard";
+import Blackjack from "../../../assets/images/PORTFOLIO PROJECTS/blackjack";
+import DynamicPortfolio from "../../../assets/images/PORTFOLIO PROJECTS/dynamic_portfolio";
+import ShoppingCart from "../../../assets/images/PORTFOLIO PROJECTS/Shopping_cart_with_smartphones";
+import BackroadsApp from "../../../assets/images/PORTFOLIO PROJECTS/BACKROADSAPP";
+import ColorApp from "../../../assets/images/PORTFOLIO PROJECTS/COLOR_generator";
+
 const projects = [
   {
     name: "Social Media Dashboard with a theme switcher",
-    img: "bla",
+    img: SocialMediaDash,
     repo: "https://github.com/Duke-Skygawker/social-media-dashboard-with-theme-switcher",
     url: "https://duke-skygawker.github.io/social-media-dashboard-with-theme-switcher/",
   },
   {
     name: "Blackjack; Player vs Dealer",
-    img: "bla",
+    img: Blackjack,
     repo: "https://github.com/Duke-Skygawker/Blackjack-player-vs-dealer",
     url: "https://duke-skygawker.github.io/Blackjack-player-vs-dealer/",
   },
   {
     name: "Dynamic Portfolio with Bootstrap 5 & SASS",
-    img: "bla",
+    img: DynamicPortfolio,
     repo: "https://github.com/Duke-Skygawker/Bootstrap-5-and-SASS-dynamic-portfolio",
     url: "#NEED_TO_HOST",
   },
   {
     name: "Shopping cart in React using a reducer",
-    img: "bla",
+    img: ShoppingCart,
     repo: "https://github.com/Duke-Skygawker/shopping-cart-react-reducer",
     url: "https://react-cart-63e6b7.netlify.app",
   },
   {
     name: "Backroads App; A Tourism company home page ",
-    img: "bla",
+    img: BackroadsApp,
     repo: "https://github.com/Duke-Skygawker/backroads-app",
     url: "https://backroads-app-67f1fc.netlify.app",
   },
   {
     name: "Color shades generator",
-    img: "bla",
+    img: ColorApp,
     repo: "https://github.com/Duke-Skygawker/color-generator-react",
     url: "https://stirring-sunshine-e5f1cf.netlify.app",
   },

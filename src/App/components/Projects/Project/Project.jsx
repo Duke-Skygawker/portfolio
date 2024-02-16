f86c6fbc86baf13dea57fcf499c40d76308910ba
Duke-Skygawker <bart3kk990@gmail.com>
2024-02-16 13:03:16 +0100

    implemented lang toggle into projects section

--- src/App/components/Projects/Project/Project.jsx
+++ src/App/components/Projects/Project/Project.jsx
@@ -1,13 +1,16 @@
+import { useGlobalContext } from "../../../../context/Context";
+
 const Project = ({ item }) => {
-  const { name, img, repo, url } = item;
+  const { showLang } = useGlobalContext();
+  const { name, namePL, img, repo, url } = item;
   return (
     <div className="project-card">
       <a href={url} target="_blank">
-        <img src={img} alt={name} />
+        <img src={img} alt={showLang === "ENG" ? name : namePL} />
       </a>
       <h3>
         <a href={repo} target="_blank">
-          {name}
+          {showLang === "ENG" ? name : namePL}
         </a>
       </h3>
     </div>

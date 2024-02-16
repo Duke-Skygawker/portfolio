c71154f45e72cbcc3f75c94d64931bd590d53aad
Duke-Skygawker <bart3kk990@gmail.com>
2024-02-16 12:56:17 +0100

    added polish names to the projects in the data array

--- src/App/components/Projects/data.js
+++ src/App/components/Projects/data.js
@@ -8,36 +8,43 @@ import ColorApp from "../../../assets/images/PORTFOLIO PROJECTS/COLOR_generator.
 const projects = [
   {
     name: "Social Media Dashboard with a theme switcher",
+    namePL:
+      "Panel Zarządzania Mediami Społecznościowymi z przełącznikiem motywów",
     img: SocialMediaDash,
     repo: "https://github.com/Duke-Skygawker/social-media-dashboard-with-theme-switcher",
     url: "https://duke-skygawker.github.io/social-media-dashboard-with-theme-switcher/",
   },
   {
     name: "Blackjack; Player vs Dealer",
+    namePL: "Blackjack; Gracz vs Rozdający",
     img: Blackjack,
     repo: "https://github.com/Duke-Skygawker/Blackjack-player-vs-dealer",
     url: "https://duke-skygawker.github.io/Blackjack-player-vs-dealer/",
   },
   {
     name: "Dynamic Portfolio with Bootstrap 5 & SASS",
+    namePL: "Dynamiczne Portfolio przy użyciu Bootstrap 5 oraz SASS",
     img: DynamicPortfolio,
     repo: "https://github.com/Duke-Skygawker/Bootstrap-5-and-SASS-dynamic-portfolio",
     url: "#NEED_TO_HOST",
   },
   {
     name: "Shopping cart in React using a reducer",
+    namePL: "Wózek zakupów w React przy użyciu reducer-a",
     img: ShoppingCart,
     repo: "https://github.com/Duke-Skygawker/shopping-cart-react-reducer",
     url: "https://react-cart-63e6b7.netlify.app",
   },
   {
     name: "Backroads App; A Tourism company home page ",
+    namePL: "Backroads App; Strona startowa firmy turystycznej",
     img: BackroadsApp,
     repo: "https://github.com/Duke-Skygawker/backroads-app",
     url: "https://backroads-app-67f1fc.netlify.app",
   },
   {
     name: "Color shades generator",
+    namePL: "Generator odcieni kolorów",
     img: ColorApp,
     repo: "https://github.com/Duke-Skygawker/color-generator-react",
     url: "https://stirring-sunshine-e5f1cf.netlify.app",

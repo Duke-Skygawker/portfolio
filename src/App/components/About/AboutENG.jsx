369d7d40ececa964514911bec08e092e26847a4f
Duke-Skygawker <bart3kk990@gmail.com>
2024-02-19 10:16:27 +0100

    about section done

---
+++ src/App/components/About/AboutENG.jsx
@@ -0,0 +1,34 @@
+const AboutENG = () => {
+  return (
+    <div className="about-container">
+      <article>
+        I have been interested in technology since I can remember. From taking
+        apart and putting together old radios, and VCR players to eventually
+        building my own Personal Computer.
+      </article>
+      <article>
+        The most dominant hobby in my life is gaming, by far. And it has been
+        that way since I've been about 4 or 5 years old. I've been wanting to
+        make games since I've been a child, and that's how I got introduced to
+        programming.
+      </article>
+      <article>
+        Because I have made some bad choices in my academic endevours, I thought
+        that I'd never be able to become a programmer, but thanks to the power
+        of the internet, and all resources that are available through it, I am
+        slowly build my programming skillset. Below you will find some of the
+        projects I have completed so far. Most of my coding endevours can be
+        found on my{" "}
+        <a href="" target="_blank">
+          github
+        </a>
+        .
+      </article>
+      <article>
+        I am currently looking for work, I have the most experience with
+        frontend technologies. React, JS, CSS, and HTML
+      </article>
+    </div>
+  );
+};
+export default AboutENG;
---
+++ src/App/components/About/AboutPL.jsx
@@ -0,0 +1,33 @@
+const AboutPL = () => {
+  return (
+    <div className="about-container">
+      <article>
+        Od zawsze interesowałem sie technologią. Od rozkręcania oraz składania
+        spowrotem starych radii oraz mangetowidów, po zbudowanie samemu własnego
+        PCeta.
+      </article>
+      <article>
+        Najgłówniejszym hobby w moim życiu jest gaming, bez porównania. I jest
+        tak odkąd byłem około 4 albo 5-cio latkiem. Chciałem robić gry, odkąd
+        byłem tylko dzieckiem, I to własnie w ten sposób odkryłem programowanie.
+      </article>
+      <article>
+        Podjąłem w życiu kilka niezbyt mądrych decyzji pod względem akademickim.
+        Przez to też myślałem, że nigdy nie zostanę programistą, ale dzięki
+        potędze internetu, oraz wszystkich zasobów do których mam dzięki niemu
+        dostęp, powoli buduję zestaw umiejętności potrzebnych programiście.
+        Poniżej można znaleźć niektóre z projektów jakie do tej pory ukończyłem.
+        Wiekszośc moich starań programistycznych można też znaleźć na moim{" "}
+        <a href="" target="_blank">
+          github
+        </a>
+        ie .
+      </article>
+      <article>
+        Aktualnie poszukuję pracy, najwięcej doświadczenia mam w technoligiach
+        frontendowych. React, JS, CSS oraz HTML
+      </article>
+    </div>
+  );
+};
+export default AboutPL;

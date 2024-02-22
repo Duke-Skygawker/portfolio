90449412be48d5dd50c306fa4ce605f88e533ff2
Duke-Skygawker <bart3kk990@gmail.com>
2024-02-22 07:47:51 +0100

    second cert added

--- src/App/components/Certifications/Certifications.jsx
+++ src/App/components/Certifications/Certifications.jsx
@@ -1,4 +1,5 @@
 import cert1 from "../../../assets/certs/freecodecamp_responsive_web_cert_censored.jpg";
+import cert2 from "../../../assets/certs/Javascript_Algorithms_and_Data_Structures_cert_censored.png";
 import { useGlobalContext } from "../../../context/Context";
 import CertHeadEng from "./CertHead/CertHeadEng";
 import CertHeadPL from "./CertHead/CertHeadPL";
@@ -12,6 +13,11 @@ const Certifications = () => {
         <div className="cert-grid">
           <img
             src={cert1}
+            alt="Responsive Web Design Certification from freeCodeCamp"
+            className="cert-img"
+          />
+          <img
+            src={cert2}
             alt="Responsive Web Design Certification from freeCodeCamp"
             className="cert-img"
           />

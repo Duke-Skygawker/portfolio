542d4a1e948b2124fe6fbc15c0c964b826c6978e
Duke-Skygawker <bart3kk990@gmail.com>
2024-05-22 09:17:59 +0200

    updated projects tile container class naming

---
+++ src/App/components/Projects/Project/ProjectLi.jsx
@@ -0,0 +1,4 @@
+const ProjectLi = () => {
+  return <div>ProjectLi</div>;
+};
+export default ProjectLi;
--- src/App/components/Projects/Projects_Tile.jsx
+++ src/App/components/Projects/Projects_Tile.jsx
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from "uuid";
 
 const ProjectsTile = () => {
   return (
-    <div className="projects-container">
+    <div className="projects-tile-container">
       {data.map((item) => {
         return <ProjectTile item={item} key={uuidv4()} />;
       })}
